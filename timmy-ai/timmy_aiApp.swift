4197ca8f6a103f7ada9f9a58884f97fe585930e6
Roman Sheydvasser <s.roman@gmail.com>
2025-12-16 15:14:39 -0500

    init commit

---
+++ timmy-ai/timmy_aiApp.swift
@@ -0,0 +1,17 @@
+//
+//  timmy_aiApp.swift
+//  timmy-ai
+//
+//  Created by Roman Sheydvasser on 12/16/25.
+//
+
+import SwiftUI
+
+@main
+struct timmy_aiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
